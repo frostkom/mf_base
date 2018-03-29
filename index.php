09ca9c2790d767ffb4bdd108017933ae6ad9f409
Martin Fors <martin.fors@frostkom.se>
2018-03-29 11:07:18 +0200

    Fix arrow up on selects

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Base
 Plugin URI: https://github.com/frostkom/mf_base
 Description: 
-Version: 8.1.0
+Version: 8.1.1
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: http://frostkom.se

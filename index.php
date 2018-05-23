c16287e9862325306fa57c4c3ec5f4297683eff6
Martin Fors <martin.fors@frostkom.se>
2018-05-23 08:55:31 +0200

    Remove webkit appearance on date/time inputs

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Base
 Plugin URI: https://github.com/frostkom/mf_base
 Description: 
-Version: 8.5.15
+Version: 8.5.16
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: http://frostkom.se

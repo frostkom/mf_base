b5c882732cc5b9dce4d89d2ecae829000b1a78e9
frostkom <martin.fors@frostkom.se>
2019-10-28 08:34:25 +0100

    Add [hidden] to style

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Base
 Plugin URI: https://github.com/frostkom/mf_base
 Description: 
-Version: 1.0.4.10
+Version: 1.0.4.11
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se

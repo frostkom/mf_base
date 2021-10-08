33a69a2f99cb35c11fe3067930bedc01e10e7f6b
frostkom <martin.fors@frostkom.se>
2021-10-08 13:19:00 +0200

    Make sure that filter for posts are active on single sites aswell

--- include/classes.php
+++ include/classes.php
@@ -2414,7 +2414,7 @@ class mf_list_table extends WP_List_Table
 				 *							'top' or 'bottom' for WP_Posts_List_Table,
 				 *							'bar' for WP_Media_List_Table.
 				 */
-				if(isset($wpdb->sitemeta) && $wpdb->sitemeta != '') // If for some reason another external DB is in use at this moment, don't bother doing this
+				if(!is_multisite() || (isset($wpdb->sitemeta) && $wpdb->sitemeta != '')) // If for some reason another external DB is in use at this moment, don't bother doing this
 				{
 					do_action('restrict_manage_posts', ($this->arr_settings['query_from'] != '' ? $this->arr_settings['query_from'] : $this->post_type), $which); //$this->screen->post_type
 				}

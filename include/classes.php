4a5274d6eec88994573be98c0ad47d50bfeaef06
frostkom <martin.fors@frostkom.se>
2023-08-24 06:05:29 +0200

    Update Swedish collation recommendation

--- include/classes.php
+++ include/classes.php
@@ -956,10 +956,11 @@ class mf_base
 					{
 						case 'sv-SE':
 							$collation_name = $wpdb->get_var($wpdb->prepare("SELECT DEFAULT_COLLATION_NAME FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = %s LIMIT 1", DB_NAME));
+							$arr_collation_name_recommended = array('utf8mb4_swedish_ci', 'latin1_swedish_ci', 'utf8mb3_swedish_ci');
 
-							if(!in_array($collation_name, array('utf8mb4_swedish_ci', 'latin1_swedish_ci', 'utf8mb3_swedish_ci')))
+							if(!in_array($collation_name, $arr_collation_name_recommended))
 							{
-								echo "<p><i class='fa fa-times red display_warning'></i> ".__("Language", 'lang_base').": ".sprintf(__("This is currently set to %s but to get the correct order on Swedish characters you should change to %s or %s", 'lang_base'), $collation_name, 'utf8mb4_swedish_ci', 'latin1_swedish_ci')."</p>";
+								echo "<p><i class='fa fa-times red display_warning'></i> ".__("Language", 'lang_base').": ".sprintf(__("This is currently set to %s but to get the correct order on Swedish characters you should change to one of these %s", 'lang_base'), $collation_name, implode(", ", $arr_collation_name_recommended))."</p>";
 							}
 						break;
 					}

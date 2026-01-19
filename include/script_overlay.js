96891b2560acdf7d23de9ba381d26404c4427d45
Martin Fors <info@martinfors.se>
2026-01-19 15:59:37 +0100

    Remove ellipsis on label and close overlay with Esc button

--- include/script_overlay.js
+++ include/script_overlay.js
@@ -4,4 +4,13 @@ jQuery(function($)
 	{
 		$(this).addClass('hide');
 	});
+
+	$(document).on("keyup", function(e)
+	{
+		/* Key up Esc */
+		if((e.which || e.keyCode) == 27)
+		{
+			$(".overlay_container.modal:not(.hide)").addClass('hide');
+		}
+	});
 });

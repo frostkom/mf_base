e58bb268d906c413c3943b5fc14014416cff6b6d
frostkom <martin.fors@frostkom.se>
2026-02-26 10:32:54 +0100

    Add support for sub tabs without affecting hash

--- include/script_tabs.js
+++ include/script_tabs.js
@@ -2,12 +2,15 @@ jQuery(function($)
 {
 	var dom_obj = $(".widget .tabs");
 
-	function open_tab(hash)
+	function open_tab(hash, update_hash)
 	{
 		dom_obj.find("a[href='" + hash + "']").parent("li").addClass('active').siblings("li").removeClass('active');
 		$(hash).addClass('active').siblings(".tab_content").removeClass('active');
 
-		history.replaceState(null, null, (location.hash != '' ? '' : '') + hash);
+		if(update_hash == true)
+		{
+			history.replaceState(null, null, (location.hash != '' ? '' : '') + hash);
+		}
 
 		return false;
 	}
@@ -16,24 +19,23 @@ jQuery(function($)
 	{
 		e.preventDefault();
 
-		open_tab($(this).attr('href'));
+		var dom_obj_tab = $(this);
+
+		open_tab(dom_obj_tab.attr('href'), (dom_obj_tab.parents(".tab_content").length == 0));
 	});
 
 	if(location.hash != '')
 	{
-		open_tab(location.hash);
+		open_tab(location.hash, false);
 	}
-
-	/*else
-	{
-		open_tab(dom_obj.find("li:first-child a").attr('href'));
-	}*/
 
 	dom_obj.each(function()
 	{
 		if($(this).children("li.active").length == 0)
 		{
-			open_tab($(this).find("li:first-child a").attr('href'));
+			var dom_obj_tab = $(this).find("li:first-child a");
+
+			open_tab(dom_obj_tab.attr('href'), (dom_obj_tab.parents(".tab_content").length == 0));
 		}
 	});
 });
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Base
 Plugin URI: https://github.com/frostkom/mf_base
 Description: Add base functionality and security improvements
-Version: 1.2.10.76
+Version: 1.2.10.77
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se

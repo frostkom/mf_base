eb976d2b3498395d1c6c4ed29e9e3190ea22257c
frostkom <martin.fors@frostkom.se>
2026-05-21 09:42:41 +0200

    Remove height/width attributes from SVG images

--- include/functions.php
+++ include/functions.php
@@ -913,6 +913,7 @@ function render_image_tag($data)
 	{
 		$image_tag = wp_get_attachment_image($data['id'], $data['size']);
 		//$image_tag = preg_replace('/(width|height)="\d*"\s?/', '', $image_tag); // This will mess up in Chrome where it will stretch images vertically
+		$image_tag = preg_replace('/(width|height)="1"\s?/', '', $image_tag); // This will make sure that SVG files does not have height="1" width="1"
 		$out .= $image_tag;
 	}
 
